60e308285329ea5ea2ad57583913a69dc1f8d680
Kevin Ludovico Mussi <kevinludovico.mussi@mail.polimi.it>
2018-08-01 17:09:37 +0200

    created classes to handle user preferences

---
+++ src/main/java/com/github/kevinmussi/itunesrp/preferences/Preferences.java
@@ -0,0 +1,31 @@
+package com.github.kevinmussi.itunesrp.preferences;
+
+import java.io.Serializable;
+
+public class Preferences implements Serializable {
+	
+	private static final long serialVersionUID = -7150420841855273495L;
+	
+	private boolean autoConnect;
+	
+	Preferences() {
+		super();
+	}
+	
+	Preferences(boolean autoConnect) {
+		this.autoConnect = autoConnect;
+	}
+	
+	public static Preferences getDefault() {
+		return new Preferences(false);
+	}
+	
+	public boolean getAutoConnect() {
+		return autoConnect;
+	}
+	
+	public void setAutoConnect(boolean autoConnect) {
+		this.autoConnect = autoConnect;
+	}
+	
+}
---
+++ src/main/java/com/github/kevinmussi/itunesrp/preferences/PreferencesManager.java
@@ -0,0 +1,80 @@
+package com.github.kevinmussi.itunesrp.preferences;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class PreferencesManager {
+	
+	private static final Logger LOGGER;
+	
+	public static final String PREFERENCES_PATH;
+	public static final String PREFERENCES_FILE;
+	
+	private static final File FILE;
+	
+	static {
+		LOGGER = Logger.getLogger(PreferencesManager.class.getName());
+		
+		PREFERENCES_PATH = System.getProperty("user.home")
+				.concat("/Library/Application Support/com.github.kevinmussi.iTunesDiscordRP/");
+		PREFERENCES_FILE = "preferences.dat";
+		
+		FILE = new File(PREFERENCES_PATH, PREFERENCES_FILE);
+		if(!FILE.exists()) {
+			setPreferences(Preferences.getDefault());
+		}
+	}
+	
+	private PreferencesManager() {
+		super();
+	}
+	
+	public static Preferences getPreferences() {
+		try(ObjectInputStream inputStream =
+				new ObjectInputStream(new FileInputStream(FILE))) {
+			Preferences pr = (Preferences) inputStream.readObject();
+			LOGGER.log(Level.INFO, "Preferences correctly read from file.");
+			return pr;
+		} catch (IOException e) {
+			LOGGER.log(Level.WARNING, "Something went wrong while reading from the preferences file: ", e);
+		} catch (ClassNotFoundException e) {
+			LOGGER.log(Level.SEVERE, "Unexpected error: ", e);
+		}
+		
+		// File doesn't exist or some error occurred
+		Preferences pr = Preferences.getDefault();
+		setPreferences(pr);
+		return pr;
+	}
+	
+	public static void setPreferences(Preferences preferences) {
+		if(preferences == null)
+			return;
+		createFile(FILE);
+		try(ObjectOutputStream outputStream =
+				new ObjectOutputStream(new FileOutputStream(FILE))) {
+			outputStream.writeObject(preferences);
+			LOGGER.log(Level.INFO, "The preferences file has been updated.");
+		} catch (IOException e) {
+			LOGGER.log(Level.WARNING, "Something went wrong while writing to the preferences file: ", e);
+		}
+	}
+	
+	private static void createFile(File file) {
+		if(!file.exists()) {
+			file.getParentFile().mkdirs();
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				LOGGER.log(Level.SEVERE, "An error occurred while creating new file: ", e);
+			}
+		}
+	}
+	
+}
