b9ff332e2a5fa087d90bfd50f8dbf96a6464be45
Kevin Mussi <kevin_495@hotmail.it>
2018-08-06 16:26:37 +0200

    modified script, moved applescript to appropriate folder

--- src/main/resources/windows/itunes_track_info_script.js
+++ src/main/resources/windows/itunes_track_info_script.js
@@ -7,68 +7,70 @@ var savedState = null;
 var state = "";
 
 function WaitForITunes() {
-	var service = WScript.CreateObject("WbemScripting.SWbemLocator").ConnectServer();
-	while(true) {
-		var items = service.ExecQuery(sQuery);
-		if(items.Count != 0) {
-			iTunesApp = WScript.CreateObject("iTunes.Application");
-			return;
-		}
-		WScript.sleep(1000);
-	}
+    var service = WScript.CreateObject("WbemScripting.SWbemLocator").ConnectServer();
+    while(true) {
+        // Search for the "iTunes.exe" process
+        var items = service.ExecQuery(sQuery);
+        if(items.Count != 0) {
+            // If there is, create the object used for retrieving the tracks and return
+            iTunesApp = WScript.CreateObject("iTunes.Application");
+            return;
+        }
+        WScript.sleep(1000);
+    }
 }
 
 function MainLoop() {
-	while(true) {
-		if(iTunesApp == null) {
-			LogStopped();
-			WaitForITunes();
-		} else {
-			try {
-				var currentTrack = iTunesApp.CurrentTrack;
-				var playerState = iTunesApp.PlayerState;
-				if(currentTrack == null) {
-					LogStopped();
-				} else if(!EqualTracks(currentTrack, savedTrack) || playerState != savedState) {
-					savedTrack = currentTrack;
-					savedState = playerState;
-					
-					if(playerState == 0) {
-						state = "PAUSED";
-					} else {
-						state = "PLAYING";
-					}
-					
-					var output = currentTrack.Name + ";;" + currentTrack.Artist + ";;" + currentTrack.Album + ";;" + state + ";;" + iTunesApp.PlayerPosition + ";;" + currentTrack.Duration + ";;" + currentTrack.TrackNumber + ";;" + currentTrack.TrackCount;
-					
-					stderr.WriteLine(output);
-				}
-				
-				WScript.sleep(1000);
-			} catch(err) {
-				iTunesApp = null;
-				savedTrack = null;
-				savedState = null;
-			}
-		}
-	}
+    while(true) {
+        if(iTunesApp == null) {
+            LogStopped();
+            WaitForITunes();
+        } else {
+            try {
+                var currentTrack = iTunesApp.CurrentTrack;
+                var playerState = iTunesApp.PlayerState;
+                if(currentTrack == null) {
+                    LogStopped();
+                } else if(!EqualTracks(currentTrack, savedTrack) || playerState != savedState) {
+                    savedTrack = currentTrack;
+                    savedState = playerState;
+                    
+                    if(playerState == 0) {
+                        state = "PAUSED";
+                    } else {
+                        state = "PLAYING";
+                    }
+                    
+                    var output = currentTrack.Name + ";;" + currentTrack.Artist + ";;" + currentTrack.Album + ";;" + state + ";;" + iTunesApp.PlayerPosition + ";;" + currentTrack.Duration + ";;" + currentTrack.TrackNumber + ";;" + currentTrack.TrackCount;
+                    stderr.WriteLine(output);
+                }
+                
+                WScript.sleep(1000);
+            } catch(err) {
+                // If iTunes stops, reset the variables and do nothing
+                iTunesApp = null;
+                savedTrack = null;
+                savedState = null;
+            }
+        }
+    }
 }
 
 function LogStopped() {
-	if(state != "STOPPED") {
-		state = "STOPPED";
-		stderr.WriteLine(state);
-	}
+    if(state != "STOPPED") {
+        state = "STOPPED";
+        stderr.WriteLine(state);
+    }
 }
 
 function EqualTracks(track1, track2) {
-	if((track1 == null && track2 != null) || (track1 != null && track2 == null)) {
-		return false;
-	} else if(track1 == null && track2 == null) {
-		return true;
-	} else {
-		return track1.Name == track2.Name && track1.Artist == track2.Artist && track1.Album == track2.Album;
-	}
+    if((track1 == null && track2 != null) || (track1 != null && track2 == null)) {
+        return false;
+    } else if(track1 == null && track2 == null) {
+        return true;
+    } else {
+        return track1.Name == track2.Name && track1.Artist == track2.Artist && track1.Album == track2.Album;
+    }
 }
 
 WaitForITunes();
